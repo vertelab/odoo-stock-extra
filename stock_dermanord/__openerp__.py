a9bf9c8d81ed7da08689f26078bbc7d1363a80a8
HaojunZou <apollo_zhj@msn.com>
2016-02-05 14:28:56 +0100

    stock_dermanord: dependence på sale_delivery_address; added new filed "shipping_address" between partner_id and weight

--- stock_dermanord/__openerp__.py
+++ stock_dermanord/__openerp__.py
@@ -29,7 +29,7 @@
 Financed by Dermanord-Svensk Hudvård AB""",
 'author': 'Vertel AB',
 'website': 'http://www.vertel.se',
-'depends': ['stock', 'delivery', 'sale_journal'],
+'depends': ['stock', 'delivery', 'sale_journal', 'sale_delivery_address'],
 'data': ['stock_view.xml'],
 'installable': True,
 }
