4c4673e2f9889581d64311e78b1bb47c9858b03e
Robin Chatfield <robin.chatfield@gmail.com>
2016-04-06 12:09:19 +0200

    stock_dermanord: dn-inventory remote zeros stock at all locations.

--- stock_dermanord/bin/dn-invetory-remote.py
+++ stock_dermanord/bin/dn-invetory-remote.py
@@ -27,49 +27,52 @@
 import erppeek
 
 #db_new = erppeek.Client.from_config('local')
-db_new = erppeek.Client.from_config('test')
-#db_new = erppeek.Client.from_config('eightzero')
-db_old = erppeek.Client.from_config('sixone')
+#db_new = erppeek.Client.from_config('test')
+db_new = erppeek.Client.from_config('eightzero')
+db_old = erppeek.Client.from_config('sixonetunnel')
+
+#Find all internal locations, except for scrap
+internal_location_ids = db_new.model('stock.location').search() #[('usage', '=', 'internal'), ('location_id', '!=', 3)])
 
 # Null qty in new database
-for prod_o in db_old.model('product.product').browse(db_old.model('product.product').search([('default_code','=','1002-00030')])):
-    prod_n = db_new.model('product.product').browse(db_old.model('product.product').search([('default_code','=',prod_o.default_code)]))[0]
-    #print "to check %s" % prod_o.default_code
-    print 'Null qty',prod_o.name
-
-    #~ l = odoo.env['stock.location'].search([('usage', '=', 'internal'), ('location_id', '!=', 3)])
-    #~ for q in odoo.env['stock.quant'].browse(odoo.env['stock.quant'].search([('product_id', '=', 4838), ('location_id', 'in', l)])):
-        #~ print q.location_id.complete_name
-
-    stock = db_new.model('stock.change.product.qty').create({
-            'product_id' : prod_n.id,
-            'new_quantity': prod_n.qty_available * -0.5,
-    })
-    print stock.change_product_qty()
-#    db_new.model('stock.quant').unlink(db_new.model('stock.quant').search([('product_id','=',prod_n.id)]))
-    print 'Null qty',prod_n.qty_available
-
-# Create stock.quant  in new database
-for prod_o in db_old.model('product.product').browse(db_old.model('product.product').search([('default_code','=','1002-00030')])):
-    prod_n = db_new.model('product.product').browse(db_old.model('product.product').search([('default_code','=',prod_o.default_code)]))[0]
-    #print "to check %s" % prod_o.default_code
-
-    move = db_old.model('stock.move').browse([db_old.model('stock.move').search([('product_id','=',prod_o.id)],order='date desc')[0]])[0]
-    #print move.name,move.location_id.name,move.location_id.location_id.name,move.location_id.location_id.id
-    parent_id = db_new.model('stock.location').search([('name','=',move.location_id.location_id.name)])[0]
-    #print 'parent',parent_id
-    location_id = db_new.model('stock.location').search(['&',('name','=',move.location_id.name),('location_id','=',parent_id)])[0]
-    print 'New qty',prod_o.name,prod_o.qty_available,location_id
-    stock = db_new.model('stock.quant').create({
-            'product_id' : prod_n.id,
-            'qty': prod_o.qty_available,
-            'location_id': location_id,
-    })
-# Check 
-for prod_o in db_old.model('product.product').browse(db_old.model('product.product').search([('default_code','=','1002-00030')])):
-    prod_n = db_new.model('product.product').browse(db_old.model('product.product').search([('default_code','=',prod_o.default_code)]))[0]
-    #print "to check %s" % prod_o.default_code
-    if not prod_o.qty_available == prod_n.qty_available:
-        print 'Check error',prod_o.name,prod_o.qty_available,prod_n.qty_available
-
+for prod_o in db_old.model('product.product').read(db_old.model('product.product').search(), ['default_code', 'name', 'qty_available']):
+    prod_ids = db_old.model('product.product').search([('default_code','=',prod_o['default_code'])])
+    if not prod_ids:
+        print "ERROR: Product not found! name: %s , default_code: %s" % (prod_o['name'], prod_o['default_code'])
+    else:
+        prod_n = db_new.model('product.product').read(prod_ids, ['id', 'name'])[0]
+        #print "to check %s" % prod_o.default_code
+        print 'Processing %s' % prod_n['name']
+        s = set()
+        ids = db_new.model('stock.quant').search([('product_id', '=', prod_n['id']), ('location_id', 'in', internal_location_ids)])
+        if ids:
+            for q in db_new.model('stock.quant').read(ids, ['location_id']):
+                s.add(q['location_id'][0])
+        for location_id in s:
+            #print db_new.model('stock.location').browse(location_id).complete_name
+            stock = db_new.model('stock.change.product.qty').create({
+                    'product_id' : prod_n['id'],
+                    'new_quantity': 0,
+                    'location_id' : location_id,
+            })
+            stock.change_product_qty()
+        print db_new.model('product.product').read(prod_ids, ['id', 'name', 'qty_available'])[0]['qty_available']
+        #    db_new.model('stock.quant').unlink(db_new.model('stock.quant').search([('product_id','=',prod_n.id)]))
+        # Create stock.quant  in new database
+        move = db_old.model('stock.move').browse([db_old.model('stock.move').search([('product_id','=',prod_o['id'])],order='date desc')[0]])[0]
+        #print move.name,move.location_id.name,move.location_id.location_id.name,move.location_id.location_id.id
+        parent_id = db_new.model('stock.location').search([('name','=',move.location_id.location_id.name)])[0]
+        #print 'parent',parent_id
+        location_id = db_new.model('stock.location').search(['&',('name','=',move.location_id.name),('location_id','=',parent_id)])[0]
+        stock = db_new.model('stock.quant').create({
+                'product_id' : prod_n['id'],
+                'qty': prod_o['qty_available'],
+                'location_id': location_id,
+        })
+        # Check 
+        prod_n = db_new.model('product.product').read(prod_ids, ['id', 'name', 'qty_available'])[0]
+        if not prod_o['qty_available'] == prod_n['qty_available']:
+            print 'Check error',prod_o['name'],prod_o['qty_available'],prod_n['qty_available']
+        else:
+            print 'Check passed',prod_o['name'],prod_o['qty_available'],prod_n['qty_available']
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
