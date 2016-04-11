b42e768a5f21179983b1b73bd39e686b2f213b70
Anders Wallenquist <aw@leia.vertel.se>
2016-04-11 09:10:12 +0200

    dn-inventory-remote: some investigation for later update

--- stock_dermanord/bin/dn-inventory-remote.py
+++ stock_dermanord/bin/dn-inventory-remote.py
@@ -34,36 +34,26 @@ db_new = erppeek.Client.from_config('eightzero')
 db_old = erppeek.Client.from_config('sixone')
 print_utf8(u"Start ♠")
 #Find all internal locations, except for scrap
-internal_location_ids = db_new.model('stock.location').search() #[('usage', '=', 'internal'), ('location_id', '!=', 3)])
+#internal_location_ids = db_new.model('stock.location').search() #[('usage', '=', 'internal'), ('location_id', '!=', 3)])
+
+# 1) hitta WH / Plocklager  och WH / Plocklager / Kallager spara i variabler
+# 2) skapa WH / Plocklager / allm och WH / Plocklager / Kallager / allm  spara i variabler
+# stock.location ('usage','=','internal'),()['&',('name','=','Plocklager'),('location_id','=',wh_id)]
 
 # Null qty in new database
+# radera alla stock.quant  testa unlink först |db_new.execute('delete from stock.quant')
+
+
+# Uppdatera lagersaldo och lagerplats
 for prod_o in db_old.model('product.product').read(db_old.model('product.product').search(), ['default_code', 'name', 'qty_available']):
-    prod_ids = db_old.model('product.product').search([('default_code','=',prod_o['default_code'])])
+    prod_ids = db_new.model('product.product').search([('default_code','=',prod_o['default_code'])])
     if not prod_ids:
         print_utf8("ERROR: Product not found! name: %s , default_code: %s" % (prod_o['name'], prod_o['default_code']))
     else:
         prod_n = db_new.model('product.product').read(prod_ids, ['id', 'name'])[0]
         #print "to check %s" % prod_o.default_code
         print_utf8('Processing %s' % prod_n['name'])
-        s = set()
-        ids = db_new.model('stock.quant').search([('product_id', '=', prod_n['id']), ('location_id', 'in', internal_location_ids)])
-        if ids:
-            for q in db_new.model('stock.quant').read(ids, ['location_id']):
-                s.add(q['location_id'][0])
-        for location_id in s:
-            try:
-                #print db_new.model('stock.location').browse(location_id).complete_name
-                stock = db_new.model('stock.change.product.qty').create({
-                    'product_id' : prod_n['id'],
-                    'new_quantity': 0,
-                    'location_id' : location_id,
-                })
-                stock.change_product_qty()
-            except:
-                print_utf8("ERROR: unable to zero %s at %s" % (prod_n['name'], location_id))
-        print db_new.model('product.product').read(prod_ids, ['id', 'name', 'qty_available'])[0]['qty_available']
-        #    db_new.model('stock.quant').unlink(db_new.model('stock.quant').search([('product_id','=',prod_n.id)]))
-        # Create stock.quant  in new database
+
         move = None
         try:
             move = db_old.model('stock.move').browse([db_old.model('stock.move').search([('product_id','=',prod_o['id'])],order='date desc')[0]])[0]
@@ -71,11 +61,20 @@ for prod_o in db_old.model('product.product').read(db_old.model('product.product
             parent_id = db_new.model('stock.location').search([('name','=',move.location_id.location_id.name)])[0]
             #print 'parent',parent_id
             location_id = db_new.model('stock.location').search(['&',('name','=',move.location_id.name),('location_id','=',parent_id)])[0]
-            stock = db_new.model('stock.quant').create({
-                    'product_id' : prod_n['id'],
-                    'qty': prod_o['qty_available'],
-                    'location_id': location_id,
+
+# om location_id == plocklager / kallager -> allm
+
+            #~ stock = db_new.model('stock.quant').create({
+                    #~ 'product_id' : prod_n['id'],
+                    #~ 'qty': prod_o['qty_available'],
+                    #~ 'location_id': location_id,
+            #~ })
+            stock = db_new.model('stock.change.product.qty').create({
+                'product_id' : prod_n['id'],
+                'new_quantity': prod_o['qty_available'],
+                'location_id' : location_id,
             })
+            stock.change_product_qty()
         except:
             print_utf8('ERROR: unable to find old location')
         # Check 
