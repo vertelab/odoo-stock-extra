f793c571152718794ecc880e3628a0ca5c8cbcc7
Robin Chatfield <robin.chatfield@gmail.com>
2016-04-20 14:05:30 +0200

    stock_dermanord: dn-inventory-remote now chooses correct stock.location in most cases. dn-migrate-products now creates all variants correctly, and moves mpr.bom along with products.

--- stock_dermanord/bin/dn-inventory-remote.py
+++ stock_dermanord/bin/dn-inventory-remote.py
@@ -26,16 +26,14 @@
 
 import erppeek
 
-
 def print_utf8(input):
     print input.encode('utf-8')
-
+print_utf8("Delete all stock.quant in 8.0 before running this script!")
 #db_new = erppeek.Client.from_config('local')
 #db_new = erppeek.Client.from_config('test')
 db_new = erppeek.Client.from_config('eightzero')
-#~ db_old = erppeek.Client.from_config('sixone')
-db_old_tunnel = erppeek.Client.from_config('sixonetunnel')
-print_utf8(u"Start ♠")
+#db_old = erppeek.Client.from_config('sixone')
+db_old = erppeek.Client.from_config('sixonetunnel')
 #Find all internal locations, except for scrap
 #internal_location_ids = db_new.model('stock.location').search() #[('usage', '=', 'internal'), ('location_id', '!=', 3)])
 
@@ -43,30 +41,34 @@ print_utf8(u"Start ♠")
 # 2) skapa WH / Plocklager / allm och WH / Plocklager / Kallager / allm  spara i variabler
 # stock.location ('usage','=','internal'),()['&',('name','=','Plocklager'),('location_id','=',wh_id)]
 
-# Null qty in new database
-# radera alla stock.quant  testa unlink först |db_new.execute('delete from stock.quant')
-
 wh_id = db_new.model('stock.location').search([('name', '=', 'WH')])[0]
 plocklager_id = db_new.model('stock.location').search(['&', ('location_id', '=', wh_id), ('name', '=', 'Plocklager')])[0]
 kallager_id = db_new.model('stock.location').search(['&', ('location_id', '=', plocklager_id), ('name', '=', 'Kallager')])[0]
 print 'WH_id: %s\nPlocklager_id: %s\nKallager_id: %s' %(wh_id, plocklager_id, kallager_id)
 
 if len(db_new.model('stock.location').search(['&', ('location_id', '=', plocklager_id), ('name', '=', 'allm')])) == 0:
-    plocklager_allm = db_new.model('stock.location').create({'name': 'allm', 'location_id': plocklager_id,})
-    print plocklager_allm
+    plocklager_allm = db_new.model('stock.location').create({'name': 'allm', 'location_id': plocklager_id,}).id
 
 else:
-    plocklager_allm = db_new.model('stock.location').search(['&', ('location_id', '=', plocklager_id), ('name', '=', 'allm')])
+    plocklager_allm = db_new.model('stock.location').search(['&', ('location_id', '=', plocklager_id), ('name', '=', 'allm')])[0]
 
 if len(db_new.model('stock.location').search(['&', ('location_id', '=', kallager_id), ('name', '=', 'allm')])) == 0:
-    kallager_allm = db_new.model('stock.location').create({'name': 'allm', 'location_id': kallager_id,})
-    print kallager_allm
+    kallager_allm = db_new.model('stock.location').create({'name': 'allm', 'location_id': kallager_id,}).id
 
 else:
-    kallager_allm = db_new.model('stock.location').search(['&', ('location_id', '=', kallager_id), ('name', '=', 'allm')])
+    kallager_allm = db_new.model('stock.location').search(['&', ('location_id', '=', kallager_id), ('name', '=', 'allm')])[0]
 
-exit()
+# Null qty in new database
+# radera alla stock.quant  testa unlink först |db_new.execute('delete from stock.quant')
 
+#Check if any stock.quant remains
+q_ids = db_new.model('stock.quant').search()
+if len(q_ids) > 0:
+    print_utf8('Failed to delete stock.quant with ids: %s' % q_ids)
+    exit()
+else:
+    print_utf8('Success! All stock.quant deleted!')
+    
 # Uppdatera lagersaldo och lagerplats
 for prod_o in db_old.model('product.product').read(db_old.model('product.product').search(), ['default_code', 'name', 'qty_available']):
     prod_ids = db_new.model('product.product').search([('default_code','=',prod_o['default_code'])])
@@ -80,13 +82,26 @@ for prod_o in db_old.model('product.product').read(db_old.model('product.product
         move = None
         try:
             move = db_old.model('stock.move').browse([db_old.model('stock.move').search([('product_id','=',prod_o['id'])],order='date desc')[0]])[0]
+            #Change to full name
+            if move.location_id.usage == 'internal':
+                location_name = move.location_id.name
+                parent_name = move.location_id.location_id.name
+            else:
+                location_name = move.location_dest_id.name
+                parent_name = move.location_dest_id.location_id.name
+            print_utf8("\tOld location: %s" % location_name)
             #print move.name,move.location_id.name,move.location_id.location_id.name,move.location_id.location_id.id
-            parent_id = db_new.model('stock.location').search([('name','=',move.location_id.location_id.name)])[0]
+            parent_id = db_new.model('stock.location').search([('name','=', parent_name)])[0]
             #print 'parent',parent_id
-            location_id = db_new.model('stock.location').search(['&',('name','=',move.location_id.name),('location_id','=',parent_id)])[0]
+            location_id = db_new.model('stock.location').search(['&', ('name', '=', location_name) ,('location_id','=',parent_id)])[0]
 
-# om location_id == plocklager / kallager -> allm
-
+            # om location_id == plocklager / kallager -> allm
+            if location_id == plocklager_id:
+                location_id = plocklager_allm
+            elif location_id == kallager_id:
+                location_id = kallager_allm
+            #Change to full name
+            print_utf8("\tNew location: %s" % db_new.model('stock.location').browse([location_id])[0].name)
             #~ stock = db_new.model('stock.quant').create({
                     #~ 'product_id' : prod_n['id'],
                     #~ 'qty': prod_o['qty_available'],
