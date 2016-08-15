dbc239da6c5ebe417a57f6983e5d42969f8fb42e
Anders Wallenquist <anders.wallenquist@vertel.se>
2016-08-15 12:34:30 +0200

    dn-iskit new

---
+++ stock_dermanord/bin/dn-iskit.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# OpenERP, Open Source Management Solution, third party addon
+# Copyright (C) 2016- Vertel AB (<http://vertel.se>).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+try:
+    import odoorpc
+except ImportError:
+    raise Warning('odoorpc library missing, pip install odoorpc')
+
+params = odoorpc.session.get('dermanord')
+odoo = odoorpc.ODOO(params.get('host'),port=params.get('port'))
+odoo.login(params.get('database'),params.get('user'),params.get('passwd'))
+
+#Find all products with x_iskit == True
+ids = odoo.env['product.template'].search([('x_iskit','=',True)])
+print "Found %s products" % len(ids)
+for id in ids:
+    print "Updating product.template %s " % id
+    odoo.env['product.template'].write(id, {'iskit': True})
