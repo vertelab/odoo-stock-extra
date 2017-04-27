ba5ffe042ee5dfd9c5a7b522f7da436154c1a024
Robin Chatfield <robin.chatfield@vertel.se>
2017-04-27 08:58:37 +0200

    stock_picking_reports: Removed modification of stock.return.picking wizard. stock_dermanord: Added missing depends. product_dermanord: Removed _name from product.product inheritance.

--- product_dermanord/product.py
+++ product_dermanord/product.py
@@ -76,7 +76,6 @@ class product_template(models.Model):
     #~ orderpoints = fields.Char(compute='_stock')
 
 class product_product(osv.osv):
-    _name = 'product.product'
     _inherit = 'product.product'
     
     _columns = {
